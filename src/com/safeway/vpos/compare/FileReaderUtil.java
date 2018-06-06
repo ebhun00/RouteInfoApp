8ce190522ebc4a081034507da901fa01226bf78a
Eswariah Bhun <ebhun00@safeway.com>
2018-06-05 17:03:46 -0700

    added  standalone program to compare VPOS files

---
+++ src/com/safeway/vpos/compare/FileReaderUtil.java
@@ -0,0 +1,18 @@
+package com.safeway.vpos.compare;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class FileReaderUtil {
+
+	public static File[] finder(String dirName) {
+		File dir = new File(dirName);
+
+		return dir.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String filename) {
+				return filename.endsWith(".dat");
+			}
+		});
+
+	}
+}
